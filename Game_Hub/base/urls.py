c10b3b5ad94edda561117a0dcf8728a9df3c4a69
MELO <MariccoA5@gmail.com>
2023-01-10 17:43:06 -0800

    file clean up

--- Game_Hub/base/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index),
-    path('login/', views.login),
-    path('signUp/', views.signup),
-    path('logout/', views.logout),
-]
--- Game_Hub/utils/model_abstracts.py
+++
@@ -1,9 +0,0 @@
-import uuid
-from django.db import models
-
-
-class Model(models.Model):
-  id = models.UUIDField(primary_key=True, defualt=uuid.uuid4)
-  
-  class Meta:
-    abstract = True
